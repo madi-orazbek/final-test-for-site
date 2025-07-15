4eec417933882fa108a44139e97c10901634684e
madi-orazbek <madiorazbek546@gmail.com>
2025-07-16 04:53:00 +0500

    Update moduleController.js

--- site-test-main/backend/controllers/moduleController.js
+++ site-test-main/backend/controllers/moduleController.js
@@ -1,18 +1,17 @@
 const Module = require('../models/Module');
 const Course = require('../models/Course');
 
-// Форма создания нового блока
 exports.newModuleForm = async (req, res) => {
   try {
     const course = await Course.findById(req.params.id);
-    res.render('modules/new', { course });
+    res.render('modules/new', { course, user: req.user });
   } catch (error) {
     console.error(error);
+    req.flash('error', 'Ошибка загрузки формы');
     res.redirect(`/courses/${req.params.id}`);
   }
 };
 
-// Создание нового блока
 exports.createModule = async (req, res) => {
   try {
     const { title } = req.body;
@@ -29,9 +28,13 @@ exports.createModule = async (req, res) => {
       $push: { modules: module._id }
     });
     
+    req.flash('success', 'Блок успешно создан');
     res.redirect(`/courses/${req.params.id}`);
   } catch (error) {
     console.error(error);
+    req.flash('error', 'Ошибка при создании блока');
     res.redirect(`/courses/${req.params.id}/modules/new`);
   }
 };
+
+// Другие методы (редактирование, удаление) можно добавить по аналогии
